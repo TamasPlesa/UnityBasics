e7d4c2d310f2d0a55a26252dd037e672c94eb16b
TamasPlesa <tamasplesa@icloud.com>
2020-12-05 23:55:03 +0100

    added unity items

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private Rigidbody rigidbodyComponent;
+    [SerializeField] private LayerMask playerMask;
+    [SerializeField] private Transform groundCheckTransform = null;
+    private int superJumpsRemaining;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbodyComponent = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+        }
+
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+    }
+
+    //FixedUpdate is called once every physic update
+    private void FixedUpdate()
+    {
+        rigidbodyComponent.velocity = new Vector3(horizontalInput * 3, rigidbodyComponent.velocity.y, 0);
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0)
+        {
+            return;
+        }
+        if (jumpKeyWasPressed)
+        {
+            float jumpPower = 4f;
+            if(superJumpsRemaining > 0)
+            {
+                jumpPower *= 2;
+                superJumpsRemaining--;
+            }
+            rigidbodyComponent.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 10)
+        {
+            Destroy(other.gameObject);
+            superJumpsRemaining++;
+        }
+    }
+
+}
